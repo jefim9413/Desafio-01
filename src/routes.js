73cbc6fa9cb7b951a16585132cccc3d95b4670ae
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-02-25 02:47:58 -0300

    finalizando rota de update (PUT)

--- src/routes.js
+++ src/routes.js
@@ -53,7 +53,29 @@ export const routes = [
         method: 'PUT',
         path:'/tasks/:id',
         handler: (req, res) => {
-            
+            const { id } = req.params
+            const { title, description } = req.body
+
+            if(!title|| !description) {
+                return res.writeHead(400).end(
+                    JSON.stringify({message: 'title or description are required' })
+                )
+            }
+
+            const [ task ] =  database.select('tasks', { id })
+
+            if(!task) {
+                return res.writeHead(404).end()
+            }
+
+            database.update('tasks', id, { 
+                title,
+                description,
+                updated_at: new Date()
+            })
+
+            return res.writeHead(204).end()
+
         }
     },
     {
